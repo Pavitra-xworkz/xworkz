04a03b970c2853912bb61a401dfc7ee315025697
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-10-21 21:37:11 +0530

    third commit

---
+++ Perform.java
@@ -0,0 +1,15 @@
+class Perform
+{
+public static void main( String [] args)
+{
+System.out.println ("main");
+music();
+}
+public static void music(){
+System.out.println("headphones");
+drinking();
+}
+public static void drinking(){
+System.out.println("water");
+}
+}
