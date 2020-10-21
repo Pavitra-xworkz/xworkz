2b7fba97b1735b876424dfe897246867283c8f76
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-10-21 21:44:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main
+{
+public static void main(String[] ags)
+{
+System.out.println("Hii");
+write();
+write();
+write();
+}
+public static void write()
+{
+System.out.println("Notes");
+}
+}
