6c614d6c1133809c8f764cb381a72897ca9d082a
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-10-21 21:45:45 +0530

    Add files via upload

---
+++ Important.java
@@ -0,0 +1,21 @@
+public class Important
+{
+public static void main(String[] args)
+{
+System.out.println("Pavitra");
+printEmail("pavitraxworkz98@gmail.com");
+printProgrammingLanguage("java pgm");
+}
+
+
+public static void printEmail(String email)
+{
+System.out.println("Email:" +email);
+}
+
+public static void printProgrammingLanguage(String programmingLangauge)
+{
+System.out.println("programmingLangauge:"+programmingLangauge);
+}
+
+}
