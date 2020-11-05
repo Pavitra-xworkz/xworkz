b92001f6ebeafbb0fe984e362ccfa7571b8e45d0
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-11-05 18:37:05 +0530

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,17 @@
+public class Location
+{
+	public static void main(String[] cnames)
+	{
+		System.out.println(" JVM invoked main");
+		
+		
+		for(int country=0;country<cnames.length;country++)
+		{
+			System.out.println("the place is "+country);
+			String nameOfState=cnames[country];
+			System.out.println("the countryname is "+nameOfcountry);
+		}
+		
+		System.out.println("EXIT :: main");
+	}
+}
