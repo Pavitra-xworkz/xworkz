e93678c01d414ca197a641e0786b635ed55dd753
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-11-05 21:32:28 +0530

    Add files via upload

---
+++ CountryTester.java
@@ -0,0 +1,11 @@
+public class CountryTester{
+	public static void main(String[] args){
+		System.out.println("jvm invokes main");
+		System.out.println("country name   "+Country.countryName);
+		System.out.println("area Of Country is   "+Country.areaOfCountry);
+		System.out.println("no of states   "+Country.noOfStates);
+		System.out.println("no of Disticts " +Country.noOfDisticts);
+		System.out.println("state name is   "+Country.stateName);
+
+	}
+}
