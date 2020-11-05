2aabed6d99501d3bac63444c4fce8ef16f552ebd
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-11-05 21:28:29 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,8 @@
+public class Address
+{
+public static short doorNo=1002;
+public static String street="Near HanumanTemple";
+public static String city="Hubli";
+public static int pinCode=582105;
+
+}
