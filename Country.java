93a81b285a73579fb6e5d8ce871d73c40840aa62
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-11-06 09:11:30 +0530

    Add files via upload

---
+++ Country.java
@@ -0,0 +1,8 @@
+  
+public class Country{
+	public static byte noOfStates=29;
+	public static short noOfDisticts=739;
+	public static String countryName="India";
+	public static int areaOfCountry=3287263;
+	public static String stateName="Karnataka";
+}
