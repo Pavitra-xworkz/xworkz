417c27d60d4cfa8849e91eb785d168259ad24ee9
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-11-06 09:10:15 +0530

    Add files via upload

---
+++ States.java
@@ -0,0 +1,7 @@
+public class States{
+	public static byte noOfDistricts=30;
+	public static short taluks=175;
+	public static String countryName="India";
+	public static long population=67562686;
+	public static String stateName="karnatak";
+}
