763d97e5a6975e380efbe4f489651dfec419e5fa
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-11-13 21:43:51 +0530

    Add files via upload

---
+++ SearchTest.java
@@ -0,0 +1,17 @@
+
+
+public class SearchTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("jvm kareyuvudu main");
+
+		System.out.println("Total items to search" + BusSearch.totalItems());
+		BusSearch.search("KSRTC");
+
+		System.out.println("main nirgamana");
+	}
+
+}
+
+
