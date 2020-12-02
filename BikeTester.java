d9500f61f6e5cedeedcbb8390d57a5bc4cebf13f
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-12-02 13:59:50 +0530

    Add files via upload

---
+++ BikeTester.java
@@ -0,0 +1,14 @@
+package com.xworkz.constructor;
+
+public class BikeTester {
+
+	public static void main(String[] args) {
+		Bike bike=new Bike("blue");
+		System.out.println(bike.color);
+		Bike bike1=new Bike("black") ;
+		System.out.println(bike1.color);
+	
+
+	}
+
+}
