d6a611e5394a4fa4cc81fb805d2070604e3225d7
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-12-02 13:54:51 +0530

    Add files via upload

---
+++ Camera.java
@@ -0,0 +1,11 @@
+package com.xworkz.memory;
+
+public class Camera {
+	public static String brand="Kodak";
+	public static String kodakFounder="George Eastman";
+	public String type="DSLR";
+	public String color="Silver";
+	public String weight="fit in pocket";
+	public String manualFocus="yes";
+}
+
