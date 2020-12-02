7584faa935c6f9347cc1c8a7d8f2f9119fb30a2e
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-12-02 14:01:41 +0530

    Add files via upload

--- CountryTester.java
+++ CountryTester.java
@@ -1,11 +1,11 @@
-public class CountryTester{
-	public static void main(String[] args){
-		System.out.println("jvm invokes main");
-		System.out.println("country name   "+Country.countryName);
-		System.out.println("area Of Country is   "+Country.areaOfCountry);
-		System.out.println("no of states   "+Country.noOfStates);
-		System.out.println("no of Disticts " +Country.noOfDisticts);
-		System.out.println("state name is   "+Country.stateName);
+package com.xworkz.constructor;
 
+public class CountryTester {
+
+	public static void main(String[] args) {
+
+	Country country=new Country("India","india.gov.in",4,28,"Narendra Modi","Hockey","Peacock");
+        
 	}
+
 }
