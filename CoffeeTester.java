9a6cb54f29741eefda0a266151ddd777e05460f6
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-12-02 14:00:48 +0530

    Add files via upload

---
+++ CoffeeTester.java
@@ -0,0 +1,10 @@
+package com.xworkz.constructor;
+
+public class CoffeeTester {
+
+	public static void main(String[] args) {
+		Coffee coffee=new Coffee("Taj",300);
+		System.out.println(coffee.coffeePowerderName);
+	}
+
+}
