544408a0cb4ee42c77c96df47113fb9f70717ea6
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-12-23 10:16:56 +0530

    Add files via upload

---
+++ browser/BrowserTester.java
@@ -0,0 +1,34 @@
+package com.xworkz.browser;
+
+import com.xworkz.browser.Browser;
+
+
+public class BrowserTester {
+
+	public static void main(String[] args) {
+
+		// data-type ref-var=new Constrctor();
+		Browser browser = new Browser();
+
+		browser.setName("IE");
+
+		System.out.println(browser.getName());
+
+		// class-name/parent-class-name ref=new Constrctor();
+
+		Object genericBrowser = new Browser();
+
+		Browser childRef = (Browser) genericBrowser;
+		childRef.setName("IE-8");
+		System.out.println(childRef.getName());
+
+		Object day = new String("TUESDAY");
+
+		String string = (String) day; // type-casting
+
+		// day=new Browser();
+		Browser notGoodRef = (Browser) day;
+
+	}
+
+}
---
+++ browser/FireBoxTester.java
@@ -0,0 +1,37 @@
+package com.xworkz.browser;
+import com.xworkz.browser.Browser;
+import com.xworkz.browser.FireBox;
+
+public class FireBoxTester {
+
+	public static void main(String[] args) {
+
+		Object generic=new FireBox();
+		
+		Browser browser=new FireBox();
+	
+		FireBox firefox=new FireBox();
+		
+		
+
+		//it is illegal Browser child=new Object();
+		
+		//instanceOf
+		generic=new String("test");
+		if(generic instanceof FireBox)
+		{
+			System.out.println("yes it is fire fox");
+			FireBox fireFoxCasted=(FireBox)generic;
+			System.out.println(firefox.getName());
+
+		}
+		else {
+			System.out.println("it is not fire fox");
+		}
+		
+		
+		
+	}
+
+}
+
